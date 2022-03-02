d59693cb62141e9104a29e9f3b1737424f51f7c7
Julia Undeutsch <JUndeutsch@gmx.at>
2022-03-02 07:59:49 +0100

    update pokedex.test with one more test

--- src/components/Pokedex.test.ts
+++ src/components/Pokedex.test.ts
@@ -3,6 +3,8 @@ import '@testing-library/jest-dom';
 import userEvent from '@testing-library/user-event';
 import ScrollArrow from './ScrollArrow';
 
+test('should render all fetched Pokemon when opening the app', () => {});
+
 test('should render arrow button when scrolling down', () => {});
 
 test('should scroll to top of page when arrow button is clicked', () => {});
