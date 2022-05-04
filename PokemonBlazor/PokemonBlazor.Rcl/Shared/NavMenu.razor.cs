d1105b01294b89dcb128da62579358f8d37b1e4f
Michael Bond <codemonkey8510@icloud.com>
2022-05-04 11:39:31 -0400

    Added Blazor project

---
+++ PokemonBlazor/PokemonBlazor.Rcl/Shared/NavMenu.razor.cs
@@ -0,0 +1,8 @@
+namespace PokemonBlazor.Rcl.Shared;
+
+public partial class NavMenu
+{
+    private bool collapseNavMenu = true;
+    private string? NavMenuCssClass => collapseNavMenu ? "collapse" : null;
+    private void ToggleNavMenu() => collapseNavMenu = !collapseNavMenu;
+}
